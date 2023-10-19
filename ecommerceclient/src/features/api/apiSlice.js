7086894e7079e30a17781737bf7b1f3972a54a8c
MaheshGuduru3 <maheshguduru30@gmail.com>
2023-10-19 20:24:46 +0530

    initial submit1

--- ecommerceclient/src/features/api/apiSlice.js
+++ ecommerceclient/src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi  , fetchBaseQuery  } from '@reduxjs/toolkit/query/react'
 export const apiSlice =  createApi({
         reducerPath:'api',
         baseQuery:fetchBaseQuery({
-           baseUrl:'http://localhost:3007/api',
+           baseUrl:`${process.env.REACT_APP_HOSTNAMED}/api`,
            prepareHeaders: (headers, { getState }) => {
                const token = (getState()).userslice.token;  
                if (token) {
